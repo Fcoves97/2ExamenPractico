c3a8b84c5518ee22ab8ba1df139386c3ec8ae010
Paco Coves <fcoves97@gmail.com>
2022-11-22 18:56:38 +0100

    Layout de la activity_introducir_valores_compra

--- app/src/main/java/pacocovesgarcia/iessochoa/a2doexamenpacocovesgarcia/IntroducirValores_Compra.kt
+++ app/src/main/java/pacocovesgarcia/iessochoa/a2doexamenpacocovesgarcia/IntroducirValores_Compra.kt
@@ -2,12 +2,10 @@ package pacocovesgarcia.iessochoa.a2doexamenpacocovesgarcia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-
+class IntroducirValores_Compra : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_introducir_valores_compra)
     }
 }
